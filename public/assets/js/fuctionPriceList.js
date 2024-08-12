df16ba48dded140e8fb7aba0c9606c70898db394
Alfredo Hurtafo <alfredocl2016@gmail.com>
2024-08-12 13:40:12 -0300

    se mejora las funciones de los checked de la lista de precios

---
+++ public/assets/js/fuctionPriceList.js
@@ -0,0 +1,26 @@
+const items = ['Bandera', 'Kilometros', 'Minutos', 'TAG', 'Peajes'];
+let inputItem;
+console.log('connect')
+
+items.map(item => {
+  inputItem = document.querySelector(`#priceList${item}Status`);
+  inputItem.addEventListener('change', () => itemChecked(item));
+});
+
+function itemChecked(element) {
+    let item = document.querySelector(`#priceList${element}`);
+    let itemCheck = document.querySelector(`#priceList${element}Status`);
+   
+    
+    if (itemCheck.value === 'on') {
+        item.value = 0;
+        item.readOnly = true;
+        item.className = 'text-danger text-end';
+        itemCheck.value = 'of';
+    } else {
+        item.value = '';
+        item.readOnly = false;
+        item.className = 'text-danger text-end';
+        itemCheck.value = 'on';
+    }
+}
